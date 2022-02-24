0f6d9f5cb50b5df1bfa22ff543ff71087d6d2bae
wdwf <w_dwf@hotmail.com>
2022-02-24 08:21:57 -0300

    first commit

---
+++ file-1.ts
@@ -0,0 +1,15 @@
+/*
+  ---TypeScript é um verificador de tipo estatico
+
+  De modo geral é feito uma detecção de erro no codigo sem
+  executa-lo, este metodo é conhecido como verificação estatica.
+  Determinar o que é um erro e o que não é baseado nos tipos de valores
+  que estão sendo operados.
+
+  TypeScript é uma linguagem que é um superconjunto de Javascript. Este
+  superconjunto é tipado, o que significa que adiciona regras sobre como
+  diferentes tipos de valores podem ser usados.
+
+  
+
+*/
