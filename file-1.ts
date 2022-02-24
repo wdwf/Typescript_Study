f58685226a9b23067169b51f37d61b6bf4da88cc
wdwf <w_dwf@hotmail.com>
2022-02-24 10:27:02 -0300

    first exercise

--- file-1.ts
+++ file-1.ts
@@ -10,6 +10,36 @@
   superconjunto é tipado, o que significa que adiciona regras sobre como
   diferentes tipos de valores podem ser usados.
 
+  Permite atuação gradual (arquivos .ts convivem com .js)
+
+
+  --POR QUE USAR TypeScript?
+
+  * Com o seu uso é possivel evitar resultados inesperados
+  pois no momento que se atribui um tipo a uma variavel
+  aquela variavel só receberá aquele tipo de valor.
+  ex: 
+    function soma(a, b) {
+      return a + b
+    }
+
+    soma(1, 2)//3
+    soma('1', '2')//12 -> Opaaa!
+
+  * Avisa sobre possiveis erros.
+    
+  --DESVANTAGENS
+
+  * Necessita ser compilado
+  * Aprendizado inicial dos tipos e boas praticas
+  * Erros nem sempre muito claros ou então muti grandes
   
 
+  --INICIANDO
+  >npm install -g typescript
+
+  --COMPILANDO
+  tsc [nome do arquivo] --watch
+
+  -> O --watch é para ficar acompanhando as mudanças
 */
