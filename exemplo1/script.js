b1193ca0774209c98df6557419d15d1bd928a50d
wdwf <w_dwf@hotmail.com>
2022-02-25 23:56:51 -0300

    knowing the types of TS

--- exemplo1/script.js
+++
@@ -1,9 +0,0 @@
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-var btn = document.getElementById("button");
-function sum(a, b) {
-    return a + b;
-}
-btn.addEventListener('click', function () {
-    console.log(sum(Number(input1.value), Number(input2.value)));
-});
--- exemplo1/script.ts
+++ exemplo1/script.ts
@@ -1,11 +1,18 @@
+//DEFININDO TIPOS
+
 const input1 = document.getElementById("num1") as HTMLInputElement;
 const input2 = document.getElementById("num2") as HTMLInputElement;
 const btn = document.getElementById("button") as HTMLInputElement;
+const result = document.getElementById("result") as HTMLInputElement;
 
 function sum(a:number, b:number) {
   return a + b;
 }
 
+/* 
+  O atributo da função requer um elemento do tipo numero
+  logo é necessario Forçar uma converção do valor do input
+*/
 btn.addEventListener('click', () => {
-  console.log(sum(Number(input1.value), Number(input2.value)));
+  result.innerHTML = `${sum(Number(input1.value), Number(input2.value))}`
 })
