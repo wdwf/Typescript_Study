fc510eb6f9a7be24d674fb2015cb60a22b9a9fc9
wdwf <w_dwf@hotmail.com>
2022-02-28 14:19:30 -0300

    more a day study

--- file-definition.ts
+++ file-definition.ts
@@ -127,7 +127,341 @@ carta = {
 
 /*
   Infere - Deduz que o tipo da variavel é referente ao tipo 
-  do valor atribuido a ela.
+  do valor atribuido a ela. Logo não é necessario definir o tipo
+  do elemento pois seria uma redundancia.  
 */
 
 let messageTwo = "mensagem definida"
+
+window.addEventListener("click", (e) => {
+  e.target
+})
+
+//### TYPE ALIASE E UNION ###//
+
+/*
+  o UNION é o uso do pipe | para se referir que o elemento pode ser
+  de um tipo ou de outro.
+
+  o ALIASE é usado quando funçoes diferentes apresentam os mesmos 
+  parametros com os mesmos tipos, assim é possivel definir e reutilizar
+*/
+
+// type alias
+type Uid = number | string | undefined
+type Platform = 'Windows' | 'Linux' | 'Mac os'
+
+function logDetails(uid: Uid, item: string) {
+  console.log(`A product with ${uid} has ${item}`);
+}
+
+function logUser(uid: Uid, user: string) {
+  console.log(`An user with ${uid} has a name as ${user}`);
+}
+
+function renderPlatform(platform: Platform) {
+  return platform
+}
+
+logDetails(123, "shoe");
+logDetails("147", "short");
+
+logUser(123, "Bob")
+logUser("147", "Jom")
+
+// renderPlatform('ios') -> erro
+
+
+//### ESTENDENDO TYPE ALIASES COM INTERSECTION ###//
+
+/*
+  o uso do ? serve para indicar que a passagem de valor para
+  determinado atributo seja opcional como se definissimos o type undefined
+
+  INTERSECTION - seria a implementação dos atibutos necessarios a um terceiro, 
+  exemplo: os atributos de A e B são necessarios para criar o elemento C 
+*/
+
+type AccountInfo = {
+  id: number;
+  name: string;
+  email?: string;
+}
+
+const accountOne: AccountInfo = {
+  id: 123,
+  name: "Rian",
+  email: "ry@email.com"
+}
+
+const accountTwo: AccountInfo = {
+  id: 123,
+  name: "Emily",
+}
+
+type CharInfo = {
+  nickname: string;
+  level: number;
+}
+
+const char: CharInfo = {
+  nickname: 'Lobomon',
+  level: 10
+}
+
+// Intersection
+type PlayerInfo = AccountInfo & CharInfo
+
+const player:PlayerInfo = {
+  id: 123,
+  name: 'Josh',
+  nickname: 'Liomon',
+  level: 100
+}
+
+
+
+//### CLASSES ###//
+
+/*
+  Modifiers - (Public, Private, Protected, Readonly)
+    Public: Metodo implicito, fornece acesso completo ao elemento
+    Private: Não permite acesso direto ao elemento
+    Protected: Só pode ser acessado pela classe q o criou
+    Readonly: Só pode ter seu valor lido 
+  
+
+  Accessor - get, set
+
+
+  Abstract class - classe abstrata é a que não possibilita criar objetos a partir dela porem permite herda-lá/extende-la
+
+    abstract class chassi {
+      ...
+    }
+*/
+
+class UserAccount {
+  public name: string; //-> propriedade que vem por padrão(implicito) e possibilita alterar valor, ler tal valor... 
+  protected age: number; //-> Só pode ser acessado dentro da classe userAccount ou das subClasses não sendo acessivel fora delas 
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  logDetails(): void {
+    console.log(`The player ${this.name} is ${this.age} years old.`);
+  }
+}
+
+//Classe que se estende classe userAccount tendo acesso a atributos e metodos
+class CharAccount extends UserAccount {
+  private nickname: string; //-> torna o elemento inacessivel diretamente
+  private level: number;   //-> disponibiliza apenas leitura do elemento
+  readonly amulet?: string
+
+  constructor(name: string, age: number, nickname: string, level: number, amulet: string) {
+    super(name, age);
+    this.nickname = nickname;
+    this.level = level;
+    this.amulet = amulet;
+  }
+
+  get getLevel() {
+    return this.level
+  }
+
+  set setLevel(level: number) {
+    this.level = level
+  }
+
+  logChar(): void {
+    console.log(`The player ${this.name} is ${this.age} and has a char ${this.nickname} at level ${this.level}`);
+  }
+}
+
+
+const bob = new UserAccount("Bob", 21);
+console.log(bob);
+// console.log(bob.age);
+bob.logDetails();
+
+const john = new CharAccount("John", 45, "JohnMaster", 80, 'Dragon Fire');
+john.logDetails();
+john.logChar();
+// john.nickname -> error nickname é privado
+console.log(john.amulet);
+// john.level = 1000 -> error level é apenas para leitura
+// john.age -> error acessivel apenas dentro da classe principal ou classe secundarias 
+
+console.log(john.getLevel);
+john.setLevel = 999;
+
+
+
+//### INTERFACES ###//
+
+/*
+  É um conjunto de dados para descrever a estrutura de um objeto
+
+  Nesta interface podemos utilizar o modifier readonly, estender com extendes
+  e implementar uma classe com interface 
+*/
+
+interface Game {
+  title : string;
+  description?: string;
+  readonly genre: string;
+  platform: string[];
+  getSimilars?: (title: string) => void
+}
+
+
+const tlou: Game = {
+  title: "The last of Us",
+  description: "The best game in the world",
+  genre: "Action",
+  platform: ["PS3", "PS4"],
+  getSimilars: (title: string) => {
+    console.log(`Similar games to ${title}: Uncharted, A Plague Tales, Metro`);
+  }
+}
+
+console.log(tlou.title);
+console.log(tlou.genre);// OK
+// tlou.genre = "Drama" // ERROR
+
+
+
+tlou.getSimilars(tlou.title)
+
+
+interface DLC extends Game {
+  originalGame: Game;
+  newContent: string[];
+}
+
+const leftbehind: DLC = {
+  title: "The Last of US - Left Behind",
+  description: "You play as Ellie before the original game",
+  genre: "action",
+  platform: ["PS4"],
+  originalGame: tlou,
+  newContent: ["3 hours story", "new characters"]
+}
+
+//Implementando uma classe com interface
+/*
+  Ao fazer isso se faz necessario ter todos os elementos
+  presentes na interface, fazendo a interface assim servir como um padrão de config 
+*/
+
+class CreateGame implements Game {
+  title: string;
+  description?: string;
+  genre: string;
+  platform: string[];
+
+  constructor(t: string, d: string, g: string, p: string[]) {
+    this.title = t;
+    this.description = d;
+    this.genre = g;
+    this.platform = p;
+  }
+}
+
+//### TYPE ALIAS VC TYPE INTERFACE ###//
+
+// DEFINIÇAO
+type HistoryT = {
+  title: string
+}
+
+type spinOffT = {
+  extra: string
+}
+
+//----
+
+interface IHistory {
+  title: string
+}
+
+interface spinOff {
+  extra: string
+}
+
+//INTERSECTION / EXTENDS
+
+//Fazendo o intersection ao realizar a criação de um HistoryCollection do mesmo tipo
+//será necessario ter o title e o extra
+type HistoryCollectionT = HistoryT & spinOffT;
+
+interface HistoryCollection extends IHistory, spinOff {}
+
+//IMPLEMENTS
+
+class CreateHistoryT implements HistoryCollectionT {
+  title: string;
+  extra: string;
+}
+
+class CreateHistory implements HistoryCollection {
+  title: string;
+  extra: string;
+}
+
+//DECLARAR FUNÇÃO
+
+type getSimilarsT = (title: string) => void;
+
+interface getSimilars {
+  (title: string): void;
+}
+
+// ================== DIFERENÇAS ======================= //
+
+//---TIPOS PRIMITIVOS
+//Com type alias é possivel declarar tipos primitivos 
+type IDT = string | number;
+
+// O que ja não é permitido fazer com interface
+// interface ID extends number {}
+
+//---TUPLAS
+// pode declarar tuplas normalmente
+type TupleT = [number, number];
+
+// [1,2,3] as TupleT -> error
+
+// Não funciona definir tuplas na interface
+interface Tuple {
+  0: number;
+  1: number;
+}
+
+// [1,2,3,4] as Tuple -> Não funciona
+
+//-- DECLARAÇÃO POR ESCOPO
+//Com Type Alias só se tem apenas uma declaração por escopo
+
+type JQueryT = { a: string };
+// type JQueryT = { b: string }; //erro
+  
+//Com interface é possivel logo possivel ter multiplas
+//declaraçoes e ele une de mesmo nome.
+//Ele faz um merge das interfaces.
+
+interface JQuery {
+  a: string;
+}
+
+interface JQuery {
+  b: string;
+}
+
+const $: JQuery = {
+  a: "bla",
+  b: "foo"
+}
