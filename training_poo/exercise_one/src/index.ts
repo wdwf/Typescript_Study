d77b283272f80af0ab1dccb586cb0f0b470db818
wdwf <w_dwf@hotmail.com>
2023-10-08 12:16:22 -0300

    :construction: chore: Add content poo

--- training_poo/exercise_one/src/index.ts
+++ training_poo/exercise_one/src/index.ts
@@ -77,7 +77,27 @@ Programação orientada a objetofoi criada com o intuito de aproximar o mundo re
     * Abstração - Seria o ato de criar uma classe ou metodo abstrato.
       Classe Abstrata ->  quando uma classe é abstrata ela não deve ser instanciada
                           uma vez que seus metodos estão incompletos
-      Metodo abstrato ->  é a definição de metodos que serão implementados por subclasses
+      Metodo abstrato ->  é apenas a definição de metodos que serão implementados por subclasses
+
+      ❗Exemplo de classe abstrata: 
+      -> Classe abstrata são as que não permitem qualquer tipo de instancia.
+      São classes feitas especialmente para serem modelos para suas classes 
+      derivadas. As classes derivadas, via de regra, deverão sobrescrever 
+      os métodos para realizar a implementação dos mesmos.
+
+      abstract class Animal {
+        abstract getHabitat();
+
+        public getRace(): string {
+          return "Race undefined"
+        }
+      }
+
+      class dog extends Animal {
+        public getHabitat():string {
+          return "house"
+        }
+      }
 
 
       exemplo de objeto da vida real
@@ -113,43 +133,91 @@ Programação orientada a objetofoi criada com o intuito de aproximar o mundo re
                 > Reservado
 
 
-** UML -> Linguagem de modelagem unificada
-    - Conceitos:
-        Diagrama de classes
-        Corpo de um objeto seguindo a uml seria seguindo o exemplo ja citado acima.
+  ** UML -> Linguagem de modelagem unificada
+      - Conceitos:
+          Diagrama de classes
+          Corpo de um objeto seguindo a uml seria seguindo o exemplo ja citado acima.
 
-Mudando a visibilidade
-        - O que é visivilidade: Indicam o nivel de acesso aos componentes (atributos/metodos) internos
-        de uma classe.
+  Mudando a visibilidade
+          - O que é visivilidade: Indicam o nivel de acesso aos componentes (atributos/metodos) internos
+          de uma classe.
 
-        Seguinda a UML podemos definir 3 simbologias
-        + -> Público (public)
-            Possibilidade de utilizar em qualquer lugar que o mesmo esteja disponivel
-            - QUEM TEM ACESSO: A classe atual e todas as outras classes.
-        - -> Privado (private)
-            Apenas O criador do componente pode utilizar
-            - QUEM TEM ACESSO: apenas a classe atual
-        # -> Protegido (protected)
-            Apenas disponivel para quem tem autorização (para os decendentes de uma classe)
-            - QUEM TEM ACESSO: a classe atual e todas as suas sub-classes
+          Seguinda a UML podemos definir 3 simbologias
+          + -> Público (public)
+              Possibilidade de utilizar em qualquer lugar que o mesmo esteja disponivel
+              - QUEM TEM ACESSO: A classe atual e todas as outras classes.
+          - -> Privado (private)
+              Apenas O criador do componente pode utilizar
+              - QUEM TEM ACESSO: apenas a classe atual
+          # -> Protegido (protected)
+              Apenas disponivel para quem tem autorização (para os decendentes de uma classe)
+              - QUEM TEM ACESSO: a classe atual e todas as suas sub-classes
 
-Classe abstrata: São as que não permitem qualquer tipo de instancia.
-São classes feitas especialmente para serem modelos para suas classes 
-derivadas. As classes derivadas, via de regra, deverão sobrescrever 
-os métodos para realizar a implementação dos mesmos.
+  ❗Nota: Quando temos um componente com a visibilidade private temos que usar metodos 
+  acessores (get/set) e este elemento só estara disponivel dentro da classe
 
-abstract class Animal {
-  abstract getHabitat();
+  ❗Nota: Quando temos um comp. com a visibilidade protected apenas a classe Pai (onde esta esta
+  sendo criado o componente) e os filhos que vem de herança poderam acessar esse elemento
 
-  public getRace(): string {
-    return "Race undefined"
-  }
-}
+  ** METODOS ESPECIAIS - METODOS ACESSORES (get) - METODOS MODIFICADORES (set)
+    São metodos que dão acesso a uma determinada coisa
+    - Getters -> Pega/Acessa algo
+    - Setters -> Modifica/Atribui algo
 
-class dog extends Animal {
-  public getHabitat():string {
-    return "house"
-  }
-}
+
+    estante = new estante()
+    meusDocs = e.getTotalDocs()
+        |-> O metodo get acessa o valor do atributo e retorna o mesmo dando assim segurança
+
+    e.setTotalDocs(doc)
+
+    --METODO CONSTRUCT
+    construct(modelo, cor, ponta) {
+      tampar()
+      cor = "Azul"
+    }
+
+    ** Como definir a visibilidade?
+
+    -- PILARES DA POO:
+    - Encapsulamento
+        > São capsulas que seguem padroes por exemplo uma pilha, existem pilhas diferentes mas todas seguem
+        um padrão de uso.
+        > Por seguir um padrão torna mudanças invisiveis
+        > Um software encapsulado ele tem um mesmo padrão, protegendo o usuario do codigo e inverso tambem.
+        > Encapsular seria ocultar partes independentes da implementação, permitindo construir partes invisiveis
+        ao mundo exterior.
+        Um bom objeto encapsulado possui uma interface bem definida
+            - Interface é a lista de serviços fornecidos por um componente. É o contato com o mundo exterior,
+            que define o que pode ser feito com um objeto dessa classe,
+            - É como se fosse a capa do controle
+
+            - UML
+            <<interface>>
+            Controlador
+            --------------
+            + ligar()
+            ...
+
+    Na interface os metodos são todos publicos e seguindo o padrao UML é definido apenas os metodos e não
+    seus funcionamentos.
+    -Uma classe implementa uma interface
+
+    E ao utilizar a interface devemos deixar os atributos do objeto por definição todos privados
+
+    - Domínio: refere-se ao conjunto de objetos, classes, métodos e conceitos que representam uma
+    área específica de conhecimento ou funcionalidade dentro de um sistema de software. O domínio é
+    essencialmente a representação das entidades e regras que são relevantes para um determinado
+    problema ou contexto.
+
+    ---------------------
+    RELACIONAMENTO ENTRE CLASSES
+
+    É o aspecto de ligar uma classe a outra.
+    existe variso tipos de relacionamento
+
+    Agregação: todo relacionamento tem um papel que seria o verbo (ter, disputar, ) (tem 1)
+
+    ---------------------
 
 */
